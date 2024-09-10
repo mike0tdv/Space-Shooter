293566dbccf9bd1b463f6d8c182078ec8475dfd9
mike0tdv <mihailtodev3@gmail.com>
2024-09-10 17:30:29 +0300

    Added healthbar

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class Player(pygame.sprite.Sprite):
         self.laser_shoot_time = 0
         self.cooldown_duration = 400 
 
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
     def laser_timer(self):
         if not self.can_shoot:
             current_time = pygame.time.get_ticks()
@@ -28,10 +31,16 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
         self.rect.center += self.direction * self.speed * dt
 
+        if self.rect.left > WINDOW_WIDTH + 100:
+            self.rect.left = -100
+        if self.rect.right < -100:
+            self.rect.right = WINDOW_WIDTH + 100
+
         if keys[pygame.K_SPACE] and self.can_shoot:
             Laser(laser_surf, self.rect.midtop, (all_sprites, laser_sprites))
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
+            laser_sound.play()
         
         self.laser_timer()
 
@@ -46,6 +55,10 @@ class Laser(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(midbottom = pos)
+        
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
     def update(self, dt):
         self.rect.centery -= 1000*dt 
         if self.rect.bottom < 0:
@@ -54,22 +67,78 @@ class Laser(pygame.sprite.Sprite):
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, groups):
         super().__init__(groups)
-        self.image = surf
+        self.original = surf
+        self.image = self.original
         self.rect = self.image.get_rect(midbottom = (randint(0, WINDOW_WIDTH), randint(-200, 0)))
         self.speed = randint(250, 600)
         self.direction = pygame.Vector2(uniform(-0.5, 0.5), 1)
-    
+        self.rotation = randint(50, 100)
+
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
     def update(self, dt):
         self.rect.center += self.direction * self.speed * dt
         if self.rect.top > WINDOW_HEIGHT:
             self.kill()
+        
+        #rotation
+        self.rotation += 100 * dt
+        self.image = pygame.transform.rotozoom(self.original, self.rotation, 1)
+        self.rect = self.image.get_rect(center = self.rect.center)
 
-def collisions():
-    pygame.sprite.spritecollide(player, meteor_sprites, True)
+class AnimatedExplosion(pygame.sprite.Sprite):
+    def __init__ (self, frames, pos, groups):
+        super().__init__(groups)
+        self.frames = frames
+        self.frames_index = 0
+        self.image = self.frames[self.frames_index]
+        self.rect = self.image.get_rect(center = pos)
+        explosion_sound.play()
+    
+    def update(self, dt):
+        self.frames_index += 20 * dt
+        if self.frames_index < len(self.frames):
+            self.image = self.frames[int(self.frames_index)]
+        else:
+            self.kill() 
+
+def healthbar(health):
+    rect = pygame.Rect(540, 650, 200, 50)
+    border = pygame.draw.rect(display_surface, "black", rect, 10, 8)
+    health_rect = pygame.Rect(550, 660, health, 30)
+    health_image = pygame.draw.rect(display_surface, "red2", health_rect)
+    
+    health, collision = player_collision(health)
+
+    if collision:
+        health_rect = pygame.Rect(550, 660, health, 30)
+
+    pygame.display.flip()
+
+
+def player_collision(health):
+    if pygame.sprite.spritecollide(player, meteor_sprites, True, pygame.sprite.collide_mask):
+        damage_sound.play()
+        health -= 20
+        return health, True
+    else: return health, False
+    
+def collisions(score):
     if len(laser_sprites) > 0:
         for laser in laser_sprites:
-            if pygame.sprite.spritecollide(laser, meteor_sprites, True):
+            if pygame.sprite.spritecollide(laser, meteor_sprites, True, pygame.sprite.collide_mask):
+                score += 1
                 laser.kill()
+                AnimatedExplosion(explosion_frames, laser.rect.midtop, all_sprites)
+                
+    return score
+
+def display_score(score):
+    text_surf = font.render(f"{score}", True, "white")
+    text_rect = text_surf.get_rect(center = (640, 600)) # !
+    pygame.draw.rect(display_surface, 'white', text_rect.inflate(20, 10).move(0, -3), 5, 10)
+    display_surface.blit(text_surf, text_rect)
 
 # GENERAL SETUP
 pygame.init()
@@ -79,11 +148,23 @@ pygame.display.set_caption("Space shooter")
 running = True
 clock = pygame.time.Clock()
 space_pressed = 0
+score = 0
+health = 180
 
 # import
 star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
 laser_surf = pygame.image.load(join("images", "laser.png")).convert_alpha()
 meteor_surf = pygame.image.load(join("images", "meteor.png")).convert_alpha()
+font = pygame.font.Font(join("images", "Oxanium-Bold.ttf"), 60)
+explosion_frames = [pygame.image.load(join("images", "explosion", f"{i}.png")).convert_alpha() for i in range(21)]
+
+laser_sound = pygame.mixer.Sound(join("audio", "laser.wav"))
+laser_sound.set_volume(0.5)
+explosion_sound = pygame.mixer.Sound(join("audio", "explosion.wav"))
+damage_sound = pygame.mixer.Sound(join("audio", "damage.ogg"))
+game_music_sound = pygame.mixer.Sound(join("audio", "game_music.wav"))
+game_music_sound.set_volume(0.3)
+game_music_sound.play(loops = True)
 
 # sprites
 all_sprites = pygame.sprite.Group()
@@ -100,6 +181,7 @@ pygame.time.set_timer(meteor_event, 500)
 
 while running:
     dt = clock.tick(60) / 1000
+
     #EVENT LOOP
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -109,11 +191,18 @@ while running:
     
     # update
     all_sprites.update(dt)
-    collisions()
+    score = collisions(score)
+    health, _ = player_collision(health)
+
+    if health <= 0:
+        running = False
 
     # DRAW THE GAME
-    display_surface.fill("black")
+    display_surface.fill("#3a2e3f")
+    display_score(score)
     all_sprites.draw(display_surface)
+    healthbar(health)   
+     
 
     pygame.display.update()
 pygame.quit()
